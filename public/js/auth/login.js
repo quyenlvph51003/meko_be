96eeafc11c691efb2ec0b677bd6be64dcb92c187
son <sonhtph51931@gmail.com>
2025-10-27 10:33:58 +0700

    update check role khi login

--- public/js/auth/login.js
+++ public/js/auth/login.js
@@ -1,28 +1,38 @@
-$('#btn_login').on('click', function(e) {
+$('#btn_login').on('click', function (e) {
   e.preventDefault();
 
   const data = {
     email: $('#username').val(),
     password: $('#password').val()
   };
+
   $.ajax({
     url: '/api/auth/login',
     method: 'POST',
     contentType: 'application/json',
     data: JSON.stringify(data),
-    success: function(res) {
+    success: function (res) {
       if (res.success) {
-        sessionStorage.setItem('token', res.data.token);
-        sessionStorage.setItem('refreshToken', res.data.refreshToken);
-
-        window.location.href = '/admin/dashboard';
+        if (res.data.role == 1) {
+          sessionStorage.setItem('token', res.data.token);
+          sessionStorage.setItem('refreshToken', res.data.refreshToken);
+          window.location.href = '/admin/dashboard';
+        } else {
+          Swal.fire({
+            icon: 'error',
+            title: 'Đăng nhập thất bại!',
+            text: 'Bạn không có quyền truy cập vào trang này!',
+            timer: 2000,
+            showConfirmButton: false
+          });
+        }
       } else {
         alert(res.message);
       }
     },
-    error: function(xhr, status, error) {
+    error: function (xhr, status, error) {
       console.error('Login error:', error);
-      alert(xhr.responseJSON.message|| error);
+      alert(xhr.responseJSON?.message || error);
     }
   });
 });
